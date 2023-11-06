bc22542869fc49d35b2a86263e81d17285c73560
SadieBeltran <etbeltran1@up.edu.ph>
2023-11-06 14:06:23 +0800

    augh

--- maybe.erl
+++ maybe.erl
@@ -3,6 +3,7 @@
 
 init_chat() -> 
     User2 = string:strip(io:get_line('Enter your name: '), right, $\n),     %% removes '\n' in name (Reference: https://stackoverflow.com/a/18573368)
+    io:format("~p started a chatroom,", [User2]),
     register(pong, spawn(maybe, pong, [User2])).
 
 %% continuous asking for message input
@@ -40,12 +41,15 @@ pongLeave(Ping_Pid) ->
 
 init_chat2(Pong_Node) ->
     User1 = string:strip(io:get_line('Enter your name: '), right, $\n),     %% removes '\n' in name (Reference: https://stackoverflow.com/a/18573368)
-    spawn(maybe, ping, [User1, Pong_Node]).
+    spawn(maybe, ping, [User1, Pong_Node, Pong_Node]).
 
 % sending Pid to pong user
-ping(User1, Pong_Node) ->
-    {pong, Pong_Node} ! self(),  
-    ping1(User1, Pong_Node).
+ping(User1, [Pong_Node | T], Pong_Nodes) ->
+    {pong, Pong_Node} ! self(),
+    ping(User1, T, Pong_Nodes);
+
+ping(User1, [], Pong_Nodes) ->
+    ping1(User1, Pong_Nodes).
 
 % continuous receiving message
 % prints the received message from the other node
@@ -63,19 +67,33 @@ ping1(User1, Pong_Node) ->
 
 % continuous asking for message input
 ping2(User1, Pong_Node) -> 
-    if
     Message = io:get_line("You: "),
+    if
         Message == "\n" ->  % ignores the empty message
             ping2(User1, Pong_Node);
         Message == "bye\n" -> pingLeave(Pong_Node);
         Message /= "bye\n" -> 
-            {pong, Pong_Node} ! {User1, Message},       % sends message to pong node
+            sendMessages(User1, Pong_Node, Pong_Node, Message),
             ping2(User1, Pong_Node)                     
-        
     end.
+
+sendMessages(User1, [Pong_Node | T], Pong_Nodes, Message) when T /= [] ->
+    {pong, Pong_Node} ! {User1, Message},       % sends message to pong node
+    sendMessages(User1, T, Pong_Nodes, Message);
+
+sendMessages(User1, [Pong_Node | []], _, Message) ->
+    {pong, Pong_Node} ! {User1, Message}.       % sends message to pong node
     
 %% when the message sent is "bye"
-pingLeave(Pong_Node) ->
+pingLeave([Pong_Node | T]) when T /= [] ->
     {pong, Pong_Node} ! bye,
+    pingLeave(T);
+
+pingLeave([T | []]) ->
+    {pong, T} ! bye,
     io:format("You left the chat~n"),
     halt(1).   % terminate process
+
+init_chat3(Nodes) ->
+    User1 = string:strip(io:get_line('Enter your name: '), right, $\n),
+    spawn(maybe, ping, [User1, Nodes, Nodes]).
